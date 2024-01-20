ffd8680f27fe99d6c55ca5b3bc76329872258d9b
mli254 <mli254@ucsc.edu>
2024-01-19 19:21:17 -0800

    scavenger hunt changes

--- Assets/Scripts/JumpScript.cs
+++ Assets/Scripts/JumpScript.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UIElements;
 
 public class JumpScript : MonoBehaviour
 {
@@ -9,6 +11,9 @@ public class JumpScript : MonoBehaviour
 
     private Rigidbody2D rb;
     public float jumpPower = 200;
+    public float movePower = 10;
+    public float maxSpeed = 50;
+    private Boolean prevDirRight = false;
 
     // Start is called before the first frame update
     void Start()
@@ -19,9 +24,30 @@ public class JumpScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.J))
         {
             rb.AddForce(jumpPower * Vector2.up);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rb.AddForce(movePower * Vector2.right);
+            prevDirRight = true;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb.AddForce(movePower * Vector2.right * -1);
+            prevDirRight = false;
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        // implementing Dash
+        {
+            if (prevDirRight)
+            {
+                rb.AddForce(movePower * Vector2.right);
+            } else 
+            {
+                rb.AddForce(movePower * Vector2.right * -1);
+            }
         }
     }
 }
