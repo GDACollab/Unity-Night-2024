d7569be6ac11d8140b7a564783d7ee2679c6f98a
Mason <maysgaming00@gmail.com>
2024-01-19 19:23:47 -0800

    Added sick freaking dashing functionality

--- Assets/Scripts/JumpScript.cs
+++ Assets/Scripts/JumpScript.cs
@@ -10,6 +10,7 @@ public class JumpScript : MonoBehaviour
     private Rigidbody2D rb;
     public float jumpPower = 200;
     public float speed = 10;
+    public float dashSpeed = 100;
 
     // Start is called before the first frame update
     void Start()
@@ -20,10 +21,16 @@ public class JumpScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Jump
         if (Input.GetKeyDown(KeyCode.Space))
         {
             rb.AddForce(jumpPower * Vector2.up);
         }
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            rb.AddForce(Vector2.right * Input.GetAxis("Horizontal") * dashSpeed);
+        }
+        // Left/Right Movement
         rb.AddForce(Vector2.right * Input.GetAxis("Horizontal") * speed);
     }
 }
